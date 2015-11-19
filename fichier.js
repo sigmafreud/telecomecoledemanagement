ea081de1886544de6b80162cc1ba363aeea25451
sigmafreud <fredericjose@yahoo.fr>
2015-11-19 15:46:00 +0100

    La cocotte de Fred
    My new website

---
+++ fichier.js
@@ -0,0 +1,12 @@
+// Custom JS
+$(document).ready(function(){
+
+	<script>
+  $('#myButton').on('click', function () {
+    var $btn = $(this).button('loading')
+    // business logic...
+    $btn.button('reset')
+  })
+	</script>
+  // Add your code here
+});
